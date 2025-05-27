b022185607a5323b2e7c16df34b4146ca0a7827a
joji <172300802+joji000@users.noreply.github.com>
2025-05-27 21:32:44 +0700

    fix:bg color layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Toaster richColors position="top-right" />
-        <div className="flex h-screen bg-gray-50">
+        <div className="flex h-screen">
           <Sidebar />
           <div className="flex-1 flex flex-col">
             <Header />
