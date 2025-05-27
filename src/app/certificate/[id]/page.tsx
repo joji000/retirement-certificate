2f3e5402c8df6b0a9674a59adfbc338c0586f52b
joji <172300802+joji000@users.noreply.github.com>
2025-05-27 21:36:50 +0700

    fix:bg color in page [id]

--- src/app/certificate/[id]/page.tsx
+++ src/app/certificate/[id]/page.tsx
@@ -18,7 +18,7 @@ export default async function CertificatePage({ params }: { params: RouteParams 
   }
 
   return (
-    <div className="min-h-screen bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-gray-100">
+    <div className="min-h-screen">
       <main className="container mx-auto">
         <CertificateDisplay certificateData={certificate} />
       </main>
